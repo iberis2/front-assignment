181b7fedbbe8711d6fb2fdc1894a13841bbae699
iberis2 <spark@blast-team.com>
2024-09-06 18:12:39 +0900

    style: run prettier

--- src/ui/component/button/index.tsx
+++ src/ui/component/button/index.tsx
@@ -6,8 +6,18 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   size?: 's' | 'm' | 'l';
   color?: 'success' | 'info' | 'error' | 'default';
-} ;
+}
 
-export default function Button({ children, size = 's', color = 'default', ...props }: Props) {
-  return <button className={classNames(S.button, S[size], S[color])} {...props}>{children}</button>;
+export default function Button({
+  children,
+  size = 's',
+  color = 'default',
+  className,
+  ...props
+}: Props) {
+  return (
+    <button className={classNames(S.button, S[size], S[color], className)} {...props}>
+      {children}
+    </button>
+  );
 }
