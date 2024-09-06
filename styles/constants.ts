75088686bc961bcc38207bd613520c2030eb30c2
iberis2 <spark@blast-team.com>
2024-09-06 14:57:12 +0900

    style: globals scss

---
+++ styles/constants.ts
@@ -0,0 +1,7 @@
+const MOBILE_QUERY = '(max-width : 767px)';
+const DESKTOP_QUERY = 'screen and (min-width: 1201px)';
+
+export const MEDIA_QUERY = {
+  desktop: DESKTOP_QUERY,
+  mobile: MOBILE_QUERY,
+};
